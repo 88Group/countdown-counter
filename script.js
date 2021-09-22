c051f5054fa35ca96341bd0dd0d23d9aeb146c51
Halim <46631802+HalimHamidov@users.noreply.github.com>
2021-09-22 12:53:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 Sept 2021";
+const newYears = "31 Dec 2021";
 
 function countdown() {
     const newYearsDate = new Date(newYears);
